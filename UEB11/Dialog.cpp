30c7ab30f800671ef642fa4a27b3bf548f2a4e87
As1259 <Schreinerandreas1@gmx.net>
2015-07-11 22:55:11 +0200

    1

---
+++ UEB11/Dialog.cpp
@@ -0,0 +1,34 @@
+#include "Dialog.h"
+#include <iostream>
+
+#include "Projekt.h"
+#include "Aufgabe.h"
+#include "Produkt.h"
+
+Dialog::Dialog()
+{
+}
+void mainDialog(){
+	bool firstRun = true;
+	Projekt *project, *teilprojekt;
+	Aufgabe *entwurf, *produktion;
+	Produkt *prototyp;
+	if (firstRun){
+		
+		firstRun = false;
+	}
+	else {
+
+	}
+}
+void createProject(Projekt* p){
+
+}
+
+void baseDialog();
+void projectDialog();
+void projectModificationDialog();
+
+Dialog::~Dialog()
+{
+}
---
+++ UEB11/Dialog.h
@@ -0,0 +1,13 @@
+#pragma once
+class Dialog
+{
+public:
+	void mainDialog();
+	void baseDialog();
+	void createProject(Projekt* p);
+	void projectDialog();
+	void projectModificationDialog();
+	Dialog();
+	~Dialog();
+};
+
